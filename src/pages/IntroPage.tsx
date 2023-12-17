725ddabe119f00876ae63125c57dad8d9735534f
Jess-Apr <jessie1944@naver.com>
2023-12-17 23:29:32 +0900

    💯 Test: Github action 작동 확인

--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -7,6 +7,7 @@ function IntroPage(): JSX.Element {
         <div className="border-b-4 border-primary-point">
           <h1 className="text-4xl font-medium tracking-wide mb-3">Introduction</h1>
         </div>
+        <div>Contents</div>
       </div>
     </div>
   );
